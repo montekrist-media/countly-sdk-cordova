a65c5506671a62f067ea0cc74840dce9802dcbb0
MikhailIgnatovich <bulich198922@gmail.com>
2023-04-07 11:40:00 +0200

    fix package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'countly:countly-sdk-js',
   version: '21.11.0', 
   summary: 'Countly is an innovative, real-time, open source mobile analytics and push notifications platform. It collects data from mobile devices, and visualizes this information to analyze mobile application usage and end-user behavior. There are two parts of Countly: the server that collects and analyzes data, and mobile SDK that sends this data. Both parts are open source with different licensing terms.',
-  git: 'https://github.com/Countly/countly-sdk-cordova.git',
+  git: 'https://github.com/montekrist-media/countly-sdk-cordova.git',
   documentation: 'README.md'
 });
 
